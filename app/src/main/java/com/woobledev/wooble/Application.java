123738d2ba30656358821f23f21787102fb05596
kahtrus <kahtrus@gmail.com>
2015-06-01 14:35:46 +0300

    first

---
+++ app/src/main/java/com/woobledev/wooble/Application.java
@@ -0,0 +1,32 @@
+package com.woobledev.wooble;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.facebook.FacebookSdk;
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+
+
+/**
+ * Created by user on 28.05.15.
+ */
+
+public class Application extends android.app.Application {
+    // Debugging switch
+    public static final boolean APPDEBUG = false;
+    private static SharedPreferences preferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+//        ParseObject.registerSubclass(AnywallPost.class);
+        Parse.initialize(this, getString(R.string.parse_app_id),
+                getString(R.string.parse_client_key));
+        ParseFacebookUtils.initialize(this);
+        preferences = getSharedPreferences("com.woobledev.wooble", Context.MODE_PRIVATE);
+
+    }
+
+}
---
+++ app/src/main/java/com/woobledev/wooble/WelcomeActivity.java
@@ -0,0 +1,52 @@
+package com.woobledev.wooble;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.facebook.FacebookSdk;
+import com.parse.ParseFacebookUtils;
+import com.woobledev.wooble.view.kbv.KenBurnsView;
+
+
+/**
+ * A login screen that offers login via email/password and via Google+ sign in.
+ * <p/>
+ * ************ IMPORTANT SETUP NOTES: ************
+ * In order for Google+ sign in to work with your app, you must first go to:
+ * https://developers.google.com/+/mobile/android/getting-started#step_1_enable_the_google_api
+ * and follow the steps in "Step 1" to create an OAuth 2.0 client for your package.
+ */
+public class WelcomeActivity extends Activity implements LoginFragment.OnFragmentInteractionListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_welcome);
+        KenBurnsView kbv = (KenBurnsView) findViewById(R.id.ken_burns_images);
+        kbv.setImageResource(R.drawable.login_background_light);
+        kbv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+//        getFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.fragment_container, new LoginFragment())
+//                .commit();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d("WelcomeActivity","OnFragmentInteractionListener " + uri);
+    }
+}
+
